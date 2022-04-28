63cf27461e59914d77ccc9bc10f19ffcb34afe16
e.kobedevon <e.kobedevon@gmail.com>
2022-04-28 12:39:04 -0500

    added responsive theme bar for bigger screens

--- src/Pages/Components/ThemePicker.jsx
+++ src/Pages/Components/ThemePicker.jsx
@@ -1,24 +1,80 @@
 import './ThemePicker.css'
+import { useState } from 'react'
 
+import {RiPaintFill , RiPaintLine} from 'react-icons/ri'
 
-
+const themes =[
+    {
+        Name: "Default",
+        Colors: ['#db4105','#000000']
+    },
+    {
+        Name: "Fp",
+        Colors: ['#2D4057','#EAEDF1']
+    },
+    {
+        Name: "Lambo",
+        Colors: ['#FF10F0','#39FF14']
+    }
+]
 
 
 
 const ThemePicker = () =>{
+    const [showBar,setShowBar] = useState(false)
+
+
+    if(!showBar)
+    {
+        return(
+        <div className="theme-picker-bar">
+            <div className="setter-holder">
+                <button className="theme-setter" onClick={()=>setShowBar(!showBar)}>
+                    <RiPaintFill/>
+                </button>
+            </div>
+        </div>
+    )
+    }
+
+
     return(
-        <div className="Theme-Bar">
-            <ThemeButton/>
-            <ThemeButton/>
+        <div className="theme-picker-bar">
+            <div className="setter-holder">
+                <button className="theme-setter turn" onClick={()=>setShowBar(!showBar)}>
+                    <RiPaintLine/>
+                </button>
+            </div>
+            <div className="theme-bar">
+                {
+                    themes.map((theme)=>{
+                        return <ThemeButton key={theme.Name} themeColors={theme.Colors}/>
+                    })
+                }
+            </div>
         </div>
     )
 }
 
 
-const ThemeButton = () =>{
+const ThemeButton = ({themeColors}) =>{
+
+    const setTheme = () =>{
+        document.documentElement.style.setProperty('--primary-color',themeColors[0])
+        document.documentElement.style.setProperty('--secondary-color',themeColors[1])
+    }
 
     return(
-        <button className="theme-btn">test</button>
+        <div className="theme-btn-holder">
+            <button className="theme-btn" onClick={setTheme}>
+                <div className="padding"></div>
+                <div className="theme-holder">
+                    <div className="semi-circle" style={{"backgroundColor":themeColors[0]}}/>
+                    <div className='semi-circle flipped' style={{"backgroundColor":themeColors[1]}}/>
+                </div>
+            </button>
+           
+        </div>
     )
 }
 
